71644425e3f81028683890ef142d4f9c6b4245a2
Angelo-Pic <78299063+Angelo-Pic@users.noreply.github.com>
2026-01-18 14:25:29 +0100

    Delete SITO directory

--- SITO/js/script.js
+++
@@ -1,5 +0,0 @@
-// CODICE PER CONTACT FORM
-document.getElementById("contattoForm").addEventListener("submit", function(e) {
-    e.preventDefault();
-    alert("Messaggio inviato correttamente!");
-});
